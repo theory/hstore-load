0424fa556e58b09122f345e545b1e029a17eac9f
David E. Wheeler <david@justatheory.com>
2013-10-22 10:26:38 -0700

    Add options to load us, uk, and ca contacts separately.
    Requires proper count to load.

--- load_tables.sh
+++ load_tables.sh
@@ -1,52 +1,112 @@
 #!/bin/bash
 
-psql <<EOF
-    CREATE TABLE us (
-        first_name TEXT,
-        last_name  TEXT,
-        company    TEXT,
-        address    TEXT,
-        city       TEXT,
-        county     TEXT,
-        state      TEXT,
-        zip        TEXT,
-        phone      TEXT,
-        fax        TEXT,
-        email      TEXT,
-        web        TEXT
-    );
 
-    CREATE TABLE ca (
-        first_name TEXT,
-        last_name  TEXT,
-        company    TEXT,
-        address    TEXT,
-        city       TEXT,
-        province   TEXT,
-        zip        TEXT,
-        phone      TEXT,
-        fax        TEXT,
-        email      TEXT,
-        web        TEXT
-    );
+USCOUNT=0
+CACOUNT=0
+UKCOUNT=0
 
-    CREATE TABLE uk (
-        id         INT,
-        first_name TEXT,
-        last_name  TEXT,
-        junk1      TEXT, 
-        junk2      TEXT,
-        email      TEXT,
-        address    TEXT,
-        city       TEXT,
-        county     TEXT,
-        zip        TEXT,
-        company    TEXT,
-        phone      TEXT,
-        web        TEXT
-    );
-EOF
+while getopts 'a:c:u:' opt; do
+    case "$opt" in
+        a)
+            case "$OPTARG" in
+                500|5000|50000|350000)
+                    USCOUNT="$OPTARG"
+                    ;;
+                *)
+                    echo >&2 "Unsupported American contact count; allowed: 500, 5000, 50000, or 35000"
+                    exit 2
+                    ;;
+            esac
+            ;;
 
-curl http://www.briandunning.com/sample-data/350000.zip | gzcat | iconv -f MacRoman -t UTF-8 | psql -d contacts -c 'COPY us FROM STDIN CSV HEADER'
-curl http://www.briandunning.com/sample-data/50000-canada.zip | gzcat | iconv -f MacRoman -t UTF-8 | psql -d contacts -c 'COPY ca FROM STDIN CSV HEADER'
-curl http://www.briandunning.com/sample-data/500-uk.zip | gzcat | iconv -f MacRoman -t UTF-8 | psql -d contacts -c 'COPY uk FROM STDIN CSV'
+        c)
+            case "$OPTARG" in
+                500|5000|50000)
+                    CACOUNT="$OPTARG"
+                    ;;
+                *)
+                    echo >&2 "Unsupported Canadian contact count; allowed: 500, 5000, or 50000"
+                    exit 2
+                    ;;
+            esac
+            ;;
+
+        u)
+            case "$OPTARG" in
+                500)
+                    UKCOUNT="$OPTARG"
+                    ;;
+                *)
+                    echo >&2 "Unsupported UK contact count; allowed: 500"
+                    exit 2
+                    ;;
+            esac
+            ;;
+    esac
+done
+
+if [ $USCOUNT != 0 ]; then
+    echo Loading $USCOUNT US contacts...
+    psql <<'    EOF'
+        CREATE TABLE us (
+            first_name TEXT,
+            last_name  TEXT,
+            company    TEXT,
+            address    TEXT,
+            city       TEXT,
+            county     TEXT,
+            state      TEXT,
+            zip        TEXT,
+            phone      TEXT,
+            fax        TEXT,
+            email      TEXT,
+            web        TEXT
+        );
+    EOF
+    curl http://www.briandunning.com/sample-data/$USCOUNT.zip | gzcat \
+        | iconv -f MacRoman -t UTF-8 | psql -c 'COPY us FROM STDIN CSV HEADER'
+fi
+
+if [ $CACOUNT != 0 ]; then
+    echo Loading $CACOUNT CA contacts...
+    psql <<'    EOF'
+        CREATE TABLE ca (
+            first_name TEXT,
+            last_name  TEXT,
+            company    TEXT,
+            address    TEXT,
+            city       TEXT,
+            province   TEXT,
+            zip        TEXT,
+            phone      TEXT,
+            fax        TEXT,
+            email      TEXT,
+            web        TEXT
+        );
+    EOF
+    curl http://www.briandunning.com/sample-data/$CACOUNT-canada.zip | gzcat \
+        | iconv -f MacRoman -t UTF-8 | psql -c 'COPY ca FROM STDIN CSV HEADER'
+fi
+
+if [ $UKCOUNT != 0 ]; then
+    echo Loading $UKCOUNT UK contacts...
+    psql <<'    EOF'
+        CREATE TABLE uk (
+            id         INT,
+            first_name TEXT,
+            last_name  TEXT,
+            junk1      TEXT, 
+            junk2      TEXT,
+            email      TEXT,
+            address    TEXT,
+            city       TEXT,
+            county     TEXT,
+            zip        TEXT,
+            company    TEXT,
+            phone      TEXT,
+            web        TEXT
+        );
+    EOF
+    curl http://www.briandunning.com/sample-data/$UKCOUNT-uk.zip | gzcat \
+        | iconv -f MacRoman -t UTF-8 | psql -c 'COPY uk FROM STDIN CSV'
+fi
